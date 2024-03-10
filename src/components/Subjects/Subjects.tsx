40ff4dea5f85ea8baa14061cfe0a267776e3d760
Illya Cherkas <deazy01025@gmail.com>
2024-03-10 12:25:05 +0200

    UNT-43: Implement skeleton loader for subjects page

---
+++ src/components/Subjects/Subjects.tsx
@@ -0,0 +1,43 @@
+import cls from './Subjects.module.scss'
+import Skeleton from '../Skeleton/Skeleton.tsx'
+import { Link } from 'react-router-dom'
+
+interface ISubject {
+  name: string
+  id: string
+}
+
+interface ISubjects {
+  title: string
+  data: ISubject[]
+}
+function Subjects({ title, data }: ISubjects) {
+  return (
+    <div className={cls.subjects}>
+      <h4>{title}</h4>
+      {data.length ? (
+        <ul className="subject_list">
+          {data?.map((el: ISubject) => (
+            <Link key={el.id} to={`/home/subjects/${el.id}`}>
+              <li className={cls.subject_item}>
+                <img src="/icons/collections_bookmark.svg" alt="Book" />
+                <div className={cls.subject_info}>
+                  <p>{el.name}</p>
+                  <img src="/icons/next_arrow.svg" alt="Go subject arrow" />
+                </div>
+              </li>
+            </Link>
+          ))}
+        </ul>
+      ) : (
+        <>
+          <Skeleton show={!data.length} height={22} width={'100%'} />
+          <Skeleton show={!data.length} height={22} width={'100%'} />
+          <Skeleton show={!data.length} height={22} width={'60%'} />
+        </>
+      )}
+    </div>
+  )
+}
+
+export default Subjects
--- src/pages/SubjectsPage/SubjectsPage.tsx
+++ src/pages/SubjectsPage/SubjectsPage.tsx
@@ -1,55 +1,22 @@
 import cls from './SubjectsPage.module.scss'
-import { Link } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
 import { fetchDataWithAxios } from '../../services/api.ts'
-
-interface ISubject {
-  name: string
-  id: string
-}
+import Subjects from '../../components/Subjects/Subjects.tsx'
 
 const SubjectsPage = () => {
-  const { data } = useQuery({
+  const { data = [] } = useQuery({
     queryFn: () => fetchDataWithAxios('/journals/all-avalible-lesssons'),
     queryKey: ['subjects'],
   })
+
+  console.log(!data.length)
 
   return (
     <div className={cls.journals_container}>
       <h1>Журнали</h1>
       <div className={cls.subject_categories}>
-        <div className={cls.general_subjects}>
-          <h4>Загально Освітні предмети</h4>
-          <ul className="subject_list">
-            {data?.map((el: ISubject, index: number) => (
-              <Link key={index + 132} to={`/home/subjects/${el.id}`}>
-                <li className={cls.subject_item}>
-                  <img src="/icons/collections_bookmark.svg" alt="Book" />
-                  <div className={cls.subject_info}>
-                    <p>{el.name}</p>
-                    <img src="/icons/next_arrow.svg" alt="Go subject arrow" />
-                  </div>
-                </li>
-              </Link>
-            ))}
-          </ul>
-        </div>
-        <div className={cls.specialized_subjects}>
-          <h4>Профільні предмети</h4>
-          <ul className="subject_list">
-            {[1, 2, 3].map((el) => (
-              <Link key={el + 232} to={'/home/subjects/123'}>
-                <li className={cls.subject_item}>
-                  <img src="/icons/collections_bookmark.svg" alt="Book" />
-                  <div className={cls.subject_info}>
-                    <p>Математика</p>
-                    <img src="/icons/next_arrow.svg" alt="Go subject arrow" />
-                  </div>
-                </li>
-              </Link>
-            ))}
-          </ul>
-        </div>
+        <Subjects data={data} title={'Загальноосвітні предмети'} />
+        <Subjects data={data} title={'Профільні предмети'} />
       </div>
     </div>
   )
