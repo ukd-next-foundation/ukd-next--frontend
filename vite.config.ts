75b8a29a96b86fa1f9016be668e50ccd11ee2678
Illya Cherkas <deazy01025@gmail.com>
2024-03-21 14:34:41 +0200

    UNT-55: Fix slow loading icons on bad internet

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{ico,png,svg}'],
+      },
       devOptions: {
         enabled: true,
       },
