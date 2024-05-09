281644d9b65588f4f272900f4099abe90cbabdd0
Illya Cherkas <deazy01025@gmail.com>
2024-05-09 10:22:08 +0300

    Schedule name error fixed

--- src/components/ScheduleSubjects/ScheduleSubject.tsx
+++ src/components/ScheduleSubjects/ScheduleSubject.tsx
@@ -22,18 +22,22 @@ function ScheduleSubject(props: IScheduleSubject) {
   return (
     <li className={classes.join(' ')}>
       <div className={cls.subject_info}>
-        <h3 className="subject_title">{lecture.lesson.name}</h3>
+        <h3 className="subject_title">{lecture?.lesson?.name || ''}</h3>
       </div>
       <div className={cls.subject_details}>
-        <small className={cls.subject_lecturer}>Вик. {lecture.teacher.fullname}</small>
+        <small className={cls.subject_lecturer}>Вик. {lecture?.teacher?.fullname}</small>
       </div>
       <div className={cls.details_info}>
         <small className={cls.subject_time}>
           {lecture.startAt.slice(0, -3)} - {lecture.endAt.slice(0, -3)}
         </small>
-        <small className={cls.subject_location}>
-          ауд. <b>{lecture.classroom.name}</b>
-        </small>
+        <>
+          {lecture?.classroom?.name && (
+            <small className={cls.subject_location}>
+              ауд. <b>{lecture.classroom.name}</b>
+            </small>
+          )}
+        </>
       </div>
     </li>
   )
