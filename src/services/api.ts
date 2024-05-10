9b56cc2decfe7498cc3aa813f5b369436f88f032
Dmytro Frame <dima.kostishen@gmail.com>
2024-05-10 12:25:09 +0300

    Fix auth acces token expired session + fix gradebook tile

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,21 +1,26 @@
 import axios from 'axios'
 import { axiosInstance } from './axios'
 
-export const refreshAccessToken = async function (refreshTokenUrl: string) {
-  const response = await axios.get(refreshTokenUrl, {
-    withCredentials: true,
-  })
-  localStorage.setItem('access-token', response.data.accessToken)
+export const refreshAccessToken = async function (refreshTokenUrl: string): Promise<null | string> {
+    return await axios.get(refreshTokenUrl, {
+      withCredentials: true,
+    })
+    .then(response => {
+      localStorage.setItem('access-token', response.data.accessToken)
+      return response.data.accessToken
+    })
+    .catch(err => {
+      console.error(err)
+      return null
+    })
 }
 
 export const fetchDataWithAxios = async function (url: string) {
   const { data } = await axiosInstance.get(url)
-  console.log('fetchDataWithAxios', data)
   return data
 }
 
 export const postDataWithAxios = async function (url: string, body: object) {
   const { data } = await axiosInstance.post(url, body)
-  console.log('postDataWithAxios', data)
   return data
 }
